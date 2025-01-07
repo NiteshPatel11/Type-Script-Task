bba5cc78ef64395a66930661980ed072a3082c8f
NiteshPatel11 <nitesh.p@liseinfotech.com>
2025-01-08 02:28:08 +0530

    Facebook signup automation with Playwright

---
+++ tests/test-data/signup-data.ts
@@ -0,0 +1,11 @@
+export const testData = {
+    validUser: {
+        firstName: 'John',
+        lastName: 'Doe',
+        email: 'john.doe@gmail.com',
+        password: 'SecurePassword123!',
+        birthDay: '14',
+        birthMonth: '6',
+        birthYear: '1990'
+    }
+}; 
